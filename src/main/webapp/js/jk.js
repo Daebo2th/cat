d74d929a8a1ef532d34be41bd61375f5dff65148
LJK <jkl02138@gmail.com>
2023-06-03 17:59:48 +0900

    jk's modified

--- src/main/webapp/js/jk.js
+++ src/main/webapp/js/jk.js
@@ -1,5 +1,8 @@
 /* jk.js */ 
 
-// document.getElementsByClassName('category').onclick =  function() {
-    
-// }
+$(document).ready(function($){
+    $('#scroll_move').click(function(event){
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    })
+})
